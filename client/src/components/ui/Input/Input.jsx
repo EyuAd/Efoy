7b37467d1cd5334354a561f45cda444b9693a3d2
halo860 <josephameha860@gmail.com>
2025-11-17 11:39:29 +0300

    chala

---
+++ client/src/components/ui/Input/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ({ label, error, ...props }) => {
+  return (
+    <div className="input-group">
+      {label && <label>{label}</label>}
+      <input {...props} />
+      {error && <span className="error">{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
