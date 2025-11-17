a374d028ed14f2590c93f08e9fca15308af4dbeb
halo860 <josephameha860@gmail.com>
2025-11-17 12:25:39 +0300

    trial

---
+++ client/src/components/layout/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Header />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
